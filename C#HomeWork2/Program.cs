2d40cbdf9c08080f8cb59c3d2d00268462fa234f
Aleksandr Novikov <alexandre.novikov@gmail.com>
2022-12-21 09:46:24 +0600

    Task 13 added

--- C#HomeWork2/Program.cs
+++ C#HomeWork2/Program.cs
@@ -1,7 +1,26 @@
-﻿//Задача №10 Напишите программу, которая принимает на вход трёхзначное 
+﻿/*Задача №10 Напишите программу, которая принимает на вход трёхзначное 
 //число и на выходе показывает вторую цифру этого числа.
 
 Console.WriteLine("Input three-digit number");
 int ThreeDigitNumber = Convert.ToInt32(Console.ReadLine() ?? "");
 string stringNumber = Convert.ToString(ThreeDigitNumber);
 Console.WriteLine ("Second digit of this number is " +stringNumber[1]);
+*/
+// Задача 13: Напишите программу, которая выводит третью цифру заданного 
+// числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine ("Input number");
+int a = Convert.ToInt32(Console.Readline() ?? "");
+
+if ((a<=100) && (a<= 999))
+{
+    Console.Writeline($"Third digit is ( a % 10");
+}
+else if (a<100)
+{
+    Console.WriteLine("There is no third digit")
+}
+else if (a > 999)
+{
+    Console.WriteLine("It is not a three digit number")
+}
