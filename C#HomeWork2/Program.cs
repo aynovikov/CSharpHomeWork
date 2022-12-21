84570bd17d40a1701d51a8212307e41e42b58a84
Aleksandr Novikov <alexandre.novikov@gmail.com>
2022-12-21 21:21:00 +0600

    Task 15 added

--- C#HomeWork2/Program.cs
+++ C#HomeWork2/Program.cs
@@ -5,7 +5,7 @@ Console.WriteLine("Input three-digit number");
 int ThreeDigitNumber = Convert.ToInt32(Console.ReadLine() ?? "");
 string stringNumber = Convert.ToString(ThreeDigitNumber);
 Console.WriteLine ("Second digit of this number is " +stringNumber[1]);
-*/
+
 // Задача 13: Напишите программу, которая выводит третью цифру заданного 
 // числа или сообщает, что третьей цифры нет.
 // 645 -> 5
@@ -27,3 +27,44 @@ else if (a > 999)
 {
     Console.WriteLine("It is not a three-digit number");
 }
+
+// Задача 15:__
+// Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+ 
+ */
+
+int NumberWeek(string message)
+{ 
+    int result = 0;
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result) && result > 0 && result < 8)
+        {
+            return result; 
+        }
+        else
+        {
+            Console.WriteLine("Wrong input. Please input a number from 1 to 7");
+        }
+    }
+
+}
+
+void numberWeek1(int NumWeek)
+{
+    if (Math.Abs(NumWeek) <= 5)
+    {
+        Console.Write("This is a Weekday");
+    }
+    else if ((NumWeek == 6) || (NumWeek == 7))
+    {
+        Console.WriteLine("This is a Day Off");
+    }
+}
+int number = NumberWeek("Enter number of the Weekday");
+numberWeek1(number);
