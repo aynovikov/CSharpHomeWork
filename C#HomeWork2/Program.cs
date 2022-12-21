202ca9a38a02d191329cefa07aa5d2060a9e251e
Aleksandr Novikov <alexandre.novikov@gmail.com>
2022-12-21 09:32:01 +0600

    Task 10 added

---
+++ C#HomeWork2/Program.cs
@@ -0,0 +1,7 @@
+﻿//Задача №10 Напишите программу, которая принимает на вход трёхзначное 
+//число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Input three-digit number");
+int ThreeDigitNumber = Convert.ToInt32(Console.ReadLine() ?? "");
+string stringNumber = Convert.ToString(ThreeDigitNumber);
+Console.WriteLine ("Second digit of this number is " +stringNumber[1]);
