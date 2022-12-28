72ff24f2d34b407cf0d39edb13a1a90d8a67fcc8
Aleksandr Novikov <alexandre.novikov@gmail.com>
2022-12-28 20:39:30 +0600

    task 19 added

---
+++ C#HomeWork3/Program.cs
@@ -0,0 +1,41 @@
+﻿//Задача 19 Напишите программу на c#, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+//14212 -> нет
+
+//12821 -> да
+
+//23432 -> да
+
+using System;
+
+namespace PalindromeChecker
+{
+class Program
+{
+static void Main(string[] args)
+{
+Console.WriteLine("Enter a five-digit number:");
+int num = Convert.ToInt32(Console.ReadLine());
+        bool isPalindrome = IsPalindrome(num);
+        if (isPalindrome)
+        {
+            Console.WriteLine("The number is a palindrome.");
+        }
+        else
+        {
+            Console.WriteLine("The number is not a palindrome.");
+        }
+    }
+
+    static bool IsPalindrome(int num)
+    {
+        // Convert the number to a string and check if it is the same forwards and backwards
+        return num.ToString() == ReverseString(num.ToString());
+    }
+
+    static string ReverseString(string s)
+    {
+        char[] arr = s.ToCharArray();
+        Array.Reverse(arr);
+        return new string(arr);
+    }
