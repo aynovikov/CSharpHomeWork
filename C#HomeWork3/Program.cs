21a221efeca4fc08163a9c48e488c18ddf347ace
Aleksandr Novikov <alexandre.novikov@gmail.com>
2022-12-28 21:02:24 +0600

    Task 21 added

--- C#HomeWork3/Program.cs
+++ C#HomeWork3/Program.cs
@@ -5,7 +5,7 @@
 //12821 -> да
 
 //23432 -> да
-
+/*
 namespace PalindromeChecker
 {
     class Program
@@ -39,4 +39,53 @@ namespace PalindromeChecker
             return new string(arr);
         }
     }
+}
+
+*/
+Напишите программу на С#, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+A (3,6,8); B (2,1,-7), -> 15.84
+
+A (7,-5, 0); B (1,-1,9) -> 11.53
+
+using System;
+
+namespace DistanceCalculator
+{
+class Program
+{
+static void Main(string[] args)
+{
+// Define the coordinates of the two points
+Point A = new Point(3, 6, 8);
+Point B = new Point(2, 1, -7);
+        // Calculate the distance between the two points
+        double distance = CalculateDistance(A, B);
+        Console.WriteLine("The distance between A and B is " + distance);
+    }
+
+    static double CalculateDistance(Point a, Point b)
+    {
+        // Calculate the distance using the distance formula
+        double dx = a.X - b.X;
+        double dy = a.Y - b.Y;
+        double dz = a.Z - b.Z;
+        return Math.Sqrt(dx * dx + dy * dy + dz * dz);
+    }
+}
+
+// Define a Point class to represent the coordinates of a point in 3D space
+class Point
+{
+    public double X { get; set; }
+    public double Y { get; set; }
+    public double Z { get; set; }
+
+    public Point(double x, double y, double z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+}
 }
