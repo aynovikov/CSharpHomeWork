d62a905faa576141181546f196f2f245edce924d
Aleksandr Novikov <alexandre.novikov@gmail.com>
2023-01-15 19:37:49 +0600

    Task 25 added

--- C#Homework4/Program.cs
+++ C#Homework4/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Task 25 Напишите цикл для C#, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+int A, B;
+int result = 1;
+
+for (int i = 0; i < B; i++)
+{
+    result *= A;
+}
+
+Console.WriteLine(result);
