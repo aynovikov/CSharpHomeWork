f4b9c8fff1d89564eddb3c28e3e49e4a58612cb2
Aleksandr Novikov <alexandre.novikov@gmail.com>
2023-01-21 16:30:50 +0600

    Task 34 solved

---
+++ C#Homework5/Program.cs
@@ -0,0 +1,23 @@
+﻿//Task 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу на C#, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] nums = new int[4];
+        Random rand = new Random();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            nums[i] =rand.Next(100, 1000);
+        }
+
+        int evenCount = nums.Count(x=> x % 2 == 0);
+        Console.WriteLine("Number of even numbers in the array: " 
+        +evenCount);
+    }
+}
