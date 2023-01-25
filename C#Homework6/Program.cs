e07d487686d5bb6913023a7c322beba2237e41c3
Aleksandr Novikov <alexandre.novikov@gmail.com>
2023-01-26 00:38:09 +0600

    Task 43 solved

--- C#Homework6/Program.cs
+++ C#Homework6/Program.cs
@@ -17,3 +17,22 @@ for (int i = 0; i < M; i++)
     }
 }
 Console.WriteLine("Number of numbers greater than 0: " + count);
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double k1, b1, k2, b2;
+Console.WriteLine("Enter the value of k1:");
+k1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter the value of b1:");
+b1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter the value of k2:");
+k2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter the value of b2:");
+b2 = double.Parse(Console.ReadLine());
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+Console.WriteLine("The point of intersection is at: (" + x + "," + y + ")");
+
