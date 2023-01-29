2a503691c7cce173d787f50f97eb5896aef903fe
Aleksandr Novikov <alexandre.novikov@gmail.com>
2023-01-29 12:02:18 +0600

    Task 58 solved

--- C#Homework8/Program.cs
+++ C#Homework8/Program.cs
@@ -56,7 +56,7 @@ class Program
 5 2 6 7
 
 Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
-*/
+
 using System;
 class MainClass 
 {
@@ -89,3 +89,51 @@ class MainClass
   }
 }
 
+
+
+//Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+//Например, даны 2 матрицы:
+//2 4 | 3 4
+//3 2 | 3 3
+//Результирующая матрица будет:
+//18 20
+//15 18
+*/
+int[,] matrix1 = new int[,] { { 2, 4 }, { 3, 2 } };
+int[,] matrix2 = new int[,] { { 3, 4 }, { 3, 3 } };
+
+int rows1 = matrix1.GetLength(0);
+int cols1 = matrix1.GetLength(1);
+int rows2 = matrix2.GetLength(0);
+int cols2 = matrix2.GetLength(1);
+
+if (cols1 != rows2)
+{
+    Console.WriteLine("Error: The number of columns of the first matrix must be equal to the number of rows of the second matrix.");
+    return;
+}
+
+int[,] resultMatrix = new int[rows1, cols2];
+
+for (int i = 0; i < rows1; i++)
+{
+    for (int j = 0; j < cols2; j++)
+    {
+        int sum = 0;
+        for (int k = 0; k < cols1; k++)
+        {
+            sum += matrix1[i, k] * matrix2[k, j];
+        }
+        resultMatrix[i, j] = sum;
+    }
+}
+
+Console.WriteLine("Result matrix:");
+for (int i = 0; i < rows1; i++)
+{
+    for (int j = 0; j < cols2; j++)
+    {
+        Console.Write(resultMatrix[i, j] + " ");
+    }
+    Console.WriteLine();
+}
