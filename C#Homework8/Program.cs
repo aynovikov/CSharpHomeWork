7f104f2988c9b12310142a7bd934b845dc81863f
Aleksandr Novikov <alexandre.novikov@gmail.com>
2023-01-29 11:44:30 +0600

    task 54 anothe solution

--- C#Homework8/Program.cs
+++ C#Homework8/Program.cs
@@ -7,9 +7,16 @@
 //7 4 2 1
 //9 5 3 2
 //8 4 4 2
-
+/*
 int[,] array = new int[3,4] {{1, 4, 7, 2}, {5, 9, 2, 3}, {8, 4, 2, 4}};
 for (int i = 0; i < array.GetLength(0); i++)
 {
     Array.Sort(array, i * array.GetLength(1), array.GetLength(1), new Comparison<int>((a, b) => b.CompareTo(a)));
+}
+*/
+int[,] array = new int[,] { { 1, 4, 7, 2 }, { 5, 9, 2, 3 }, { 8, 4, 2, 4 } };
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    Array.Sort(array, i * array.GetLength(1), array.GetLength(1), Comparer<int>.Create((x, y) => y - x));
 }
