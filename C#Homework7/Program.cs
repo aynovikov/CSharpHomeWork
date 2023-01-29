e6ba5683a20ebffeb5d9e93b29b24b029a3bd188
Aleksandr Novikov <alexandre.novikov@gmail.com>
2023-01-29 10:33:05 +0600

    Corrected

--- C#Homework7/Program.cs
+++ C#Homework7/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿//Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами
+//m = 3, n = 4.
+//0,5 7 -2 -0,2
+//1 -3,3 8 -9,9
+//8 7,8 -7,1 9
+
+using System;
 using System.Linq;
 
 class Program
@@ -9,8 +15,6 @@ int m = 3, n = 4;
 var rand = new Random();
 double[,] arr = new double[m, n];
 
-css
-Copy code
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
