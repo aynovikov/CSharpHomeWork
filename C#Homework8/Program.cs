1e0fed6bfaf814d9d8ed3c5682bef9d6f251204f
Aleksandr Novikov <alexandre.novikov@gmail.com>
2023-01-29 11:48:35 +0600

    Task 54 soved

--- C#Homework8/Program.cs
+++ C#Homework8/Program.cs
@@ -7,16 +7,39 @@
 //7 4 2 1
 //9 5 3 2
 //8 4 4 2
-/*
-int[,] array = new int[3,4] {{1, 4, 7, 2}, {5, 9, 2, 3}, {8, 4, 2, 4}};
-for (int i = 0; i < array.GetLength(0); i++)
-{
-    Array.Sort(array, i * array.GetLength(1), array.GetLength(1), new Comparison<int>((a, b) => b.CompareTo(a)));
-}
-*/
-int[,] array = new int[,] { { 1, 4, 7, 2 }, { 5, 9, 2, 3 }, { 8, 4, 2, 4 } };
 
-for (int i = 0; i < array.GetLength(0); i++)
+/*
+using System;
+using System.Linq;
+
+class Program
 {
-    Array.Sort(array, i * array.GetLength(1), array.GetLength(1), Comparer<int>.Create((x, y) => y - x));
+    static void Main(string[] args)
+    {
+        int[,] arr = new int[,] { { 1, 4, 7, 2 }, { 5, 9, 2, 3 }, { 8, 4, 2, 4 } };
+
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            int[] row = new int[arr.GetLength(1)];
+            for (int j = 0; j < arr.GetLength(1); j++)
+            {
+                row[j] = arr[i, j];
+            }
+            Array.Sort(row);
+            Array.Reverse(row);
+            for (int j = 0; j < arr.GetLength(1); j++)
+            {
+                arr[i, j] = row[j];
+            }
+        }
+
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            for (int j = 0; j < arr.GetLength(1); j++)
+            {
+                Console.Write(arr[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
 }
