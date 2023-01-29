32796af15986b07a0ab98165dbf0c7bb5a246fcf
Aleksandr Novikov <alexandre.novikov@gmail.com>
2023-01-29 10:31:46 +0600

    Task 47 solved

---
+++ C#Homework7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+static void Main()
+{
+int m = 3, n = 4;
+var rand = new Random();
+double[,] arr = new double[m, n];
+
+css
+Copy code
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            arr[i, j] = rand.NextDouble();
+        }
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
