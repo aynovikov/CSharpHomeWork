c099bc7d9a6fa02a3656d10a3eaf413324aa12d9
Aleksandr Novikov <alexandre.novikov@gmail.com>
2023-02-01 14:35:23 +0600

    Task 66 solved

--- C#Homework9/Program.cs
+++ C#Homework9/Program.cs
@@ -2,7 +2,7 @@
 
 //N = 5 -> "5, 4, 3, 2, 1"
 //N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1
-
+/*
 using System;
 
 class Program
@@ -22,3 +22,31 @@ class Program
         CountDown(N);
     }
 }
+*/
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+
+using System;
+
+class Program
+{
+    static int SumNaturalNumbers(int m, int n)
+    {
+        if (m > n)
+            return 0;
+
+        return m + SumNaturalNumbers(m + 1, n);
+    }
+
+    static void Main(string[] args)
+    {
+        int M = 1;
+        int N = 15;
+        Console.WriteLine(SumNaturalNumbers(M, N));
+    }
+}
+
+
